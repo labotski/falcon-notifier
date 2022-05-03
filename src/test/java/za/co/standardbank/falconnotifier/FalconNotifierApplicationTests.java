8067e2c074e93372c7622b95691baeb5cc37861f
Siarhei Labotski <siarhei.labotski@kyndryl.com>
2022-05-03 17:38:46 +0300

    falcon-date-every-date

--- src/test/java/za/co/standardbank/falconnotifier/FalconNotifierApplicationTests.java
+++ src/test/java/za/co/standardbank/falconnotifier/FalconNotifierApplicationTests.java
@@ -1,9 +1,7 @@
 package za.co.standardbank.falconnotifier;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FalconNotifierApplicationTests {
 
 	@Test
